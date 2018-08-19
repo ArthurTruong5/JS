054d329d1d6af2c45be5687ed6d8c81f03fca93e
arthurtruong <arthurtruong.work@gmail.com>
2018-08-19 19:32:14 +1000

    callback,scope,async,es6

---
+++ weird-parts/scope-es6-and-let.js
@@ -0,0 +1,11 @@
+// Scope, ES6, and let
+
+// Scope
+// Where a variable is available in your code. e.g same or new copy?
+
+// let
+// lets allows the JavaScript engine use block scoping.
+
+if (a > b) {
+  let c = true;
+}
