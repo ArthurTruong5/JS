15a8575d42d327da72d48cfc649602b4211c6fb7
arthurtruong <arthurtruong.work@gmail.com>
2018-08-21 16:20:38 +1000

    objects

---
+++ weird-parts/objects-and-functions/objects-and-object-literals.js
@@ -0,0 +1,26 @@
+var Tony = {
+  firstname: 'Tony',
+  lastname: 'Truong',
+  address: {
+    street: '111 Main St.',
+    city: 'New York',
+    state: 'NY'
+  }
+};
+
+function greet(person) {
+  console.log('Hi ' + person.firstname);
+}
+
+greet(Tony);
+
+greet({
+  firstname: 'Mary',
+  lastname: 'Doe'
+});
+
+Tony.address2 = {
+
+  street: '333 Second St.'
+
+};
--- weird-parts/objects-and-functions/objects-and-the-dot.js
+++ weird-parts/objects-and-functions/objects-and-the-dot.js
@@ -1,28 +1,28 @@
-// Objects and the dot
-
-// Objects
-// 1. Primitve "property" - 0x002
-// 2. Object "property" - 0x003
-// 3. Function "method" - 0x004
-
- var person = new Object();
-
- person["firstname"] = "Tony";
- person["lastname"] = "Alicea";
-
- var firstNameProperty = "firstname";
-
- console.log(person);
- console.log(person[firstNameProperty]);
-
- console.log(person.firstname);
- console.log(person.lastname);
-
-person.address = new Object();
-person.address.street = "111 Main St.";
-person.address.city = "New York";
-person.address.state = "NY";
-
-
-console.log(person.address);
-console.log(person["address"]["state"]);
+// // Objects and the dot
+//
+// // Objects
+// // 1. Primitve "property" - 0x002
+// // 2. Object "property" - 0x003
+// // 3. Function "method" - 0x004
+//
+//  var person = new Object();
+//
+//  person["firstname"] = "Tony";
+//  person["lastname"] = "Alicea";
+//
+//  var firstNameProperty = "firstname";
+//
+//  console.log(person);
+//  console.log(person[firstNameProperty]);
+//
+//  console.log(person.firstname);
+//  console.log(person.lastname);
+//
+// person.address = new Object();
+// person.address.street = "111 Main St.";
+// person.address.city = "New York";
+// person.address.state = "NY";
+//
+//
+// console.log(person.address);
+// console.log(person["address"]["state"]);
