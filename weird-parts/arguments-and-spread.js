219b552fe8e9192a266fa92cd1710298cbf39fd8
arthurtruong <arthurtruong.work@gmail.com>
2018-08-23 18:59:03 +1000

    arrays,arguments,spread

---
+++ weird-parts/arguments-and-spread.js
@@ -0,0 +1,12 @@
+// Big word alert
+// Arguments: The parameters you pass to a function. Javascript gives you a keyword of the same name which contains them all.
+
+function greet(firstname, lastname, language) {
+  console.log(firstname);
+  console.log(lastname);
+  console.log(language);
+  console.log('--------');
+}
+
+greet();
+greet('John');
